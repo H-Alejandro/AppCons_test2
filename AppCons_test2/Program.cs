7622e6f88426d375d6fd4c6fb355212c956caab9
HECTOR HURTADO <hector.hurtado@pucese.edu.ec>
2020-05-25 09:41:16 -0500

    Ejemplo condicional

---
+++ AppCons_test2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AppCons_test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Ejemplo de los participantes de los pilares de la programación estructurada 
+            //secuencias
+
+            int a;
+            a = 10;
+            int b = 20;
+            Console.WriteLine("La multiplicación de {0} x {1} es {2} ", a, b, (a * b));
+            Console.WriteLine("Pulse una tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
